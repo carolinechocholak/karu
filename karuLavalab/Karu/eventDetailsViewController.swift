844f7a43f69c55dda26712bdb95876a806758e34
Caroline Chocholak <carolinechocholak@guest-wireless-nup-1616-10-121-007-161.usc.edu>
2018-04-22 17:21:06 -0700

    added create team page

--- karuLavalab/Karu/eventDetailsViewController.swift
+++ karuLavalab/Karu/eventDetailsViewController.swift
@@ -32,6 +32,7 @@ class eventDetailsViewController: UIViewController {
     @IBOutlet weak var NotGoingButton: UIButton!
     @IBOutlet weak var GoingButton: UIButton!
     @IBOutlet weak var updateButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
     
     @IBOutlet weak var announcementsDetail: UILabelPadding!
     
@@ -41,8 +42,25 @@ class eventDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
          super.viewDidLoad()
-       
+        self.titleLabel.text = "Cycle For Survival"
         
+        let updateColor = UIColor(displayP3Red: 89.0/255.0, green: 183.0/255.0, blue: 211.0/255.0, alpha: 1.0)
+        self.updateButton.setTitleColor(updateColor, for: UIControlState.normal)
+        self.updateButton.layer.borderColor = updateColor.cgColor
+        self.updateButton.layer.borderWidth = 1.5
+        self.updateButton.layer.cornerRadius = 2.0
+        
+        let goingColor = UIColor(displayP3Red: 82.0/255.0, green: 196.0/255.0, blue: 69.0/255.0, alpha: 1.0)
+        self.GoingButton.setTitleColor(goingColor, for: UIControlState.normal)
+        self.GoingButton.layer.borderColor = goingColor.cgColor
+        self.GoingButton.layer.borderWidth = 1.5
+        self.GoingButton.layer.cornerRadius = 2.0
+        
+        let notGoingColor = UIColor(displayP3Red: 208.0/255.0, green: 2.0/255.0, blue: 27.0/255.0, alpha: 1.0)
+        self.NotGoingButton.setTitleColor(notGoingColor, for: UIControlState.normal)
+        self.NotGoingButton.layer.borderColor = notGoingColor.cgColor
+        self.NotGoingButton.layer.borderWidth = 1.5
+        self.NotGoingButton.layer.cornerRadius = 2.0
         
         
        
@@ -80,10 +98,16 @@ class eventDetailsViewController: UIViewController {
     
 
     @IBAction func goingTouched(_ sender: Any) {
-        let alertController = UIAlertController(title: "You have just changed your status to this event to Going, is this correct?", message: "This is an alert.", preferredStyle: .alert)
-        
+        let alertController = UIAlertController(title: "You have just changed your status to this event to Going, is this correct?", message: "", preferredStyle: .alert)
+        let backView = alertController.view.subviews.last?.subviews.last
+        backView?.layer.cornerRadius = 10.0
+        backView?.backgroundColor = UIColor.white
         let action1 = UIAlertAction(title: "Confirm", style: .default) { (action:UIAlertAction) in
             print("You've pressed default");
+            self.GoingButton.isHidden = true
+            self.NotGoingButton.isHidden = true
+            self.updateButton.isHidden = false
+            self.titleLabel.text = "Cycle For Survival:\nGoing"
         }
         
         
@@ -95,6 +119,29 @@ class eventDetailsViewController: UIViewController {
       
         self.present(alertController, animated: true, completion: nil)
     }
+    
+    @IBAction func NotGoingTouched(_ sender: Any) {
+        let alertController = UIAlertController(title: "You have just changed your status to this event to Not Going, is this correct?", message: "", preferredStyle: .alert)
+        
+        let action1 = UIAlertAction(title: "Confirm", style: .default) { (action:UIAlertAction) in
+            print("You've pressed default");
+            self.GoingButton.isHidden = true
+            self.NotGoingButton.isHidden = true
+            self.updateButton.isHidden = false
+            self.titleLabel.text = "Cycle For Survival:\nNot Going"
+        }
+        
+        
+        let action2 = UIAlertAction(title: "Back", style: .destructive) { (action:UIAlertAction) in
+            print("You've pressed the destructive");
+        }
+        alertController.addAction(action1)
+        alertController.addAction(action2)
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
     /*
     // MARK: - Navigation
 
